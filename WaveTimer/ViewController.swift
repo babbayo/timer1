17fc229930d83b5ad3fe73ff856c300332e163f7
babbayo <na.babbayo@gmail.com>
2019-12-28 23:06:50 +0900

    Initial Commit

---
+++ WaveTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaveTimer
+//
+//  Created by MacBook on 28/12/2019.
+//  Copyright © 2019 yaco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
